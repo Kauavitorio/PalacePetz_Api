3584cf875650d80a7df634f059897695b3f4b694
kaua vitorio <kauavitorioof@gmail.com>
2021-05-03 19:57:24 -0300

    Create new methods for update image and user information (#15)

--- controllers/userController.js
+++ controllers/userController.js
@@ -113,3 +113,59 @@ exports.UpdateAddress = async (req, res, next) => {
         return res.status(500).send( { error: error } )
     }
 }
+
+//Method for Update Profile Image
+exports.UpdateProfileImage = async (req, res, next) => {
+    try{
+        var query;
+        var URL_ProfileImage = EncryptDep.Encrypto(req.body.img_user)
+        var ID_User = req.body.id_user
+        query = `SELECT * FROM tbl_account WHERE id_user = ?`
+        var result = await mysql.execute(query, ID_User)
+
+        if(result.length > 0){
+            if(URL_ProfileImage != "" || URL_ProfileImage.length >= 8 || URL_ProfileImage != null){
+                query = 'UPDATE tbl_account SET img_user = ? WHERE id_user = ?'
+                await mysql.execute(query, [URL_ProfileImage, ID_User])
+                return res.status(200).send({ message: 'Update Image successfully !!' })
+            }
+        }else{
+            return res.status(404).send({ message: 'User not registered' })
+        }
+    }catch(error){
+        ServerDetails.RegisterServerError("Update Profile Image", error.toString());
+        return res.status(500).send( { error: error } )
+    }
+}
+
+//Method for Update Profile
+exports.UpdateProfile = async (req, res, next) => {
+    try{
+        var ID_USER = req.body.id_user
+        var query = `SELECT * FROM tbl_account WHERE id_user = ? `
+        var result = await mysql.execute(query, ID_USER)
+        if(result.length > 0){
+            if(BadWords.VerifyUsername(req.body.name_user)){
+                return res.status(406).send({ error: "Username not allowed"})                
+            }else{
+                query = `UPDATE tbl_account SET 
+                name_user = ?,
+                cpf_user = ?,
+                address_user = ?, 
+                complement = ?, 
+                zipcode = ?, 
+                phone_user = ?, 
+                birth_date = ?, 
+                img_user = ? 
+                    WHERE id_user = ?`
+                await mysql.execute(query, [EncryptDep.Encrypto(req.body.name_user), EncryptDep.Encrypto(req.body.cpf_user), EncryptDep.Encrypto(req.body.address_user), EncryptDep.Encrypto(req.body.complement), EncryptDep.Encrypto(req.body.zipcode), EncryptDep.Encrypto(req.body.phone_user), EncryptDep.Encrypto(req.body.birth_date), EncryptDep.Encrypto(req.body.img_user), ID_USER])
+                return res.status(200).send( { message: 'User information successfully update'} )
+            }
+        }else{
+            return res.status(404).send( { message: 'User not registered' } )
+        }
+    }catch(error){
+        ServerDetails.RegisterServerError("Update Profile", error.toString());
+        return res.status(500).send( { error: error } )
+    }
+}
