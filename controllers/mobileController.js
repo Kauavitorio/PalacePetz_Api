79ec1f9dd787913613e94c24074d8ad5d0819ed9
kaua vitorio <kauavitorioof@gmail.com>
2021-05-16 23:26:03 -0300

    Fix mobile version

--- controllers/mobileController.js
+++ controllers/mobileController.js
@@ -4,15 +4,16 @@ const ServerDetails = require('../ServerInfo')
 exports.GetMobileVersion = async (req, res, next) => {
     try {
         ServerDetails.showRequestId()
-        var result = mysql.execute('SELECT * FROM tbl_versionMobile')
+        var result = await mysql.execute('select * from tbl_versionMobile;')
         if(result.length > 0){
             return res.status(200).send({
-                cd_version: result.cd_version,
-                versionName: result.versionName,
-                versionCode: result.versionCode
+                cd_version: result[0].cd_version,
+                versionName: result[0].versionName,
+                versionCode: result[0].versionCode
             })
-        }else
-        return res.status(204).send({ message: 'No version on database' })
+        }else{
+            return res.status(204).send({ message: 'No version on database' })
+        }
     } catch (error) {
         ServerDetails.RegisterServerError("Get Mobile Version", error.toString());
         return res.status(500).send({ error: error.toString()})
